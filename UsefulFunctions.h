7095273df3cb48603052fce37792468e3b0e8c1c
Matt Larson <larson.matt.b@gmail.com>
2015-12-02 22:48:39 -0600

    Just added comments.

--- UsefulFunctions.h
+++ UsefulFunctions.h
@@ -12,11 +12,23 @@ namespace FinalProject {
     class UsefulFunctions {
     public:
         UsefulFunctions();
+
+        //Handles answers to yes/no questions
         bool Yes_No_Question();
+
+        //Converts strings to upper case.
         std::string changeToUpper(std::string myInput);
+
+        //Takes a seat's letter as a string and returns the int value
         int getIntFromSeatLetter(std::string seatLetterUsed);
+
+        //Takes the int value of a seat's letter and returns the string value
         std::string getSeatLetterFromInt(int seatInt);
+
+        //Takes an int and returns the string value
         std::string getStringfromInt(int myInput);
+
+        //Calculates the difference between two dates (time between the dates)
         int Days_Difference(time_t first_date, time_t second_date);
     };
 }
